b119baa3f153a7a32eef039ced60c689e95ac4a3
Shane Lee <shanelee007@gmail.com>
2021-04-22 14:17:44 +1000

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  context: path.join(__dirname, 'src'),
+  entry: {
+    test1: './test1.ts', 
+    test2: './test2.ts'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    libraryTarget: 'commonjs',
+    filename: '[name].js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  target: 'web',
+  externals: /^k6(\/.*)?/,
+  stats: {
+    colors: true,
+  },
+};
