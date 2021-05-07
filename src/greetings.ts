5c480c1f7c358df924932ffa00716a539c2a2b40
Shane Lee <shanelee007@gmail.com>
2021-05-07 16:31:23 +1000

    threshold

--- src/greetings.ts
+++ src/greetings.ts
@@ -16,7 +16,7 @@ export let options: Options = {
   //if you want to fail the whole load test use thresholds
   thresholds: {
     http_req_failed: ["rate<0.01"], // http errors should be less than 1%
-    http_req_duration: ["p(95)<310"], // 95% of requests should be below 200ms
+    http_req_duration: ["p(95)<350"], // 95% of requests should be below 200ms
   },
   // httpDebug: "true",
   userAgent: "K6GreetingsDemo/1.0",
